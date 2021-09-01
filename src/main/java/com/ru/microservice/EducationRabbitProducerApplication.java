e109d3723ca54ae3e01fbe4e72cf00da19f78421
eugeneosis <fckngbld75@gmail.com>
2021-09-01 21:45:41 +0300

    Init commit

---
+++ src/main/java/com/ru/microservice/EducationRabbitProducerApplication.java
@@ -0,0 +1,13 @@
+package com.ru.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationRabbitProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EducationRabbitProducerApplication.class, args);
+    }
+
+}
