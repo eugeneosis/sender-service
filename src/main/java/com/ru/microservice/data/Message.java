759629c0600d3d46d734e14fcce0755084414295
eugeneosis <fckngbld75@gmail.com>
2021-09-02 10:56:43 +0300

    Implemented producer pattern

---
+++ src/main/java/com/ru/microservice/data/Message.java
@@ -0,0 +1,20 @@
+package com.ru.microservice.data;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import java.sql.Timestamp;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Message {
+
+    String id;
+    String message;
+    Timestamp dateTime;
+}
